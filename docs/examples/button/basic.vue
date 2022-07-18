e97b62730abcdb11e1deb967c2466bddc937ad45
yaoly <yaoly@getui.com>
2022-07-18 08:29:19 +0800

    feat: 组件样式替换

--- docs/examples/button/basic.vue
+++ docs/examples/button/basic.vue
@@ -1,6 +1,6 @@
 <template>
   <el-row class="mb-4">
-    <el-button>Default</el-button>
+    <el-button type="default">Default</el-button>
     <el-button type="primary">Primary</el-button>
     <el-button type="success">Success</el-button>
     <el-button type="info">Info</el-button>
